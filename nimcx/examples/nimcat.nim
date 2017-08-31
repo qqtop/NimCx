1b995119f87b52ff61d0086990e7d2af54a7b21b
qqtop <pict100@gmail.com>
2017-08-31 23:05:26 +0800

    Tiny improvements

--- nimcx/examples/nimcat.nim
+++ nimcx/examples/nimcat.nim
@@ -1,16 +1,18 @@
-import os,nimcx
+import os,nimcx,osproc
 
 # nimcat   
 # a cat for nim :)
-# usage : nimcat test4 test1     # shows 2 nim files 
-# all other file types need the full filename like e.g. notes.txt
+# usage : nimcat test4 sometext somenumbers someothertext
+# file ext other than .nim  need the full filename like e.g. notes.txt
+if paramCount() > 0:
+        var cp1 = newseq[TaintedString]()
+        cp1 = commandLineParams()
+        if cp1.len > 0:
+            nimcat(paramStr(1),countphrase = cp1[1 .. <cp1.len])
+        else:
+            nimcat(paramStr(1))
+else:
+     printLnBiCol("Error : No Filename to list specified",red,termwhite,":",0,false,{})
+     printLnBiCol("Usage : nimcat filename  <optional someword number> ",greenyellow,termwhite,":",0,false,{})
 
-
-var pc = paramcount()
-if pc > 0:
-    for x in 0.. <pc:
-      nimcat(paramStr(x + 1))
-hlineLn()      
-printLnBiCol("Files Shown: " & $pc,colLeft=peru)
-echo()
 doFinish()
