18261eac91f9b462cbfec9b50454510a7c8978c2
qqtop <nomail@nomail.com>
2020-09-30 13:13:17 +0100

    Tiny improvements

--- nimcx/cxzenity.nim
+++ nimcx/cxzenity.nim
@@ -2,15 +2,16 @@ import nimcx
 
 # Experimental
 #
-# zenity driven interfaces 
-# zenity can be installed on linux
-# maybe will become part of nimcx
+# zenity driven interfaces for gtk based messageboxes
+# zenity can be installed on linux , comes with many gnome based distros
+# has more options than whiptail a similar app
 #
-# Last  : 2020-09-26
+# Last  : 2020-09-30
 #
 
 
 proc cxZDialog*(title:string="cxZDialog",text:string="Demo Text",hideText:bool=false ):string =
+   ## cxZDialog
    if hideText == false:
       let zs = """zenity --entry --title="$1" --text="$2" """ % [title,text]
       var dialg = execCmdEx(zs)
@@ -22,8 +23,8 @@ proc cxZDialog*(title:string="cxZDialog",text:string="Demo Text",hideText:bool=f
       dialg.output.removeSuffix('\n')
       result = dialg.output
   
-proc cxLoginDialog*(title:string="Login Dialog",text:string="Login"):tuple[name:string,password:string] =
-     ## cxloginDialog
+proc cxZLoginDialog*(title:string="Login Dialog",text:string="Login"):tuple[name:string,password:string] =
+     ## cxZLoginDialog
      ##
      ## pressing cancel exits application
      ##
@@ -36,6 +37,8 @@ proc cxLoginDialog*(title:string="Login Dialog",text:string="Login"):tuple[name:
         let fs = forms.split(";")
         result.name = fs[0]
         result.password = fs[1] 
+
+
 
 
 proc cxLoginEncDialog*(title:string="Login Dialog",text:string="Login",enckeytext:string = ""):tuple[name:string,password:string,enckey:string] =
@@ -61,19 +64,24 @@ proc cxLoginDateDialog*(title:string="Login Dialog",text:string="Login"):tuple[n
      ##
      ## username,password,date dialog
      ##
-     var forms = execCmdEx("""zenity --forms --add-entry=Username --add-password=Password --add-calendar=Date --text="WriteBash.com Demo Form" --separator=';' --forms-date-format=%Y/%m/%d""").output
+     var forms = execCmdEx("""zenity --forms --add-entry=Username --add-password=Password --add-calendar=Date --text=$1 --separator=';' --forms-date-format=%Y/%m/%d  % [text] """).output
      forms.removeSuffix('\n')
      if forms == "":
         printLnAlertMsg("Login failed ! Incorrect credentials supplied. ")
         doFinish()
      else:    
-        var fs = forms.split(";")
+        let fs = forms.split(";")
         result.name = fs[0]
         result.password = fs[1] 
         result.date = fs[2] 
         
-proc cxZInfo*(infotext:string="Yes Stay_calm_carry_on") = 
-   discard  execCmdEx("""zenity --info --text="$1" --no-wrap""" % infotext)
+proc cxZInfo*(infotext:string="Yes Stay_calm_carry_on",width:int=300) = 
+   discard  execCmdEx("""zenity --info --text="$1" --no-wrap --width=$2""" % [infotext,$width])
+ 
+
+proc cxZYesNo*(text:string="",width:int=300):string =
+    let rp = execCmdEx("""zenity --question --text "$1 ?" --ok-label "Yes" --cancel-label="No" --width=$2 """ % [text,$width])  #.output
+    result = if rp.exitcode == 0: "yes" else: "no"  # yes = 0 no = 1 
    
 proc cxZQuestion*(ask:string = "How Are you ?",cancel:string="No",ok:string="Yes"):bool =
     ## cxZQuestion
@@ -86,11 +94,11 @@ proc cxZQuestion*(ask:string = "How Are you ?",cancel:string="No",ok:string="Yes
     else:
        result = false
 
-proc cxZWarn*(warntext:string=" This is a warning ! Hope you head it ! ") =
+proc cxZWarn*(warntext:string=" This is a warning ! Hope you head it ! ",width:int=250) =
      ## cxZWarn   
      ##
      ## display a warning message
-     discard execCmd("""zenity --warning --text="$1" --no-wrap"""  % warntext)
+     discard execCmd("""zenity --warning --text="$1" --no-wrap" --width=$2"""  % [warntext,$width])
      
 proc cxZError*(errortext:string=" An error has occured ")=    
      ## cxError
@@ -99,15 +107,15 @@ proc cxZError*(errortext:string=" An error has occured ")=
      discard execCmdEx("""zenity --error --text="$1" --no-wrap"""  % errortext)
 
 
-proc cxZDisplayFile*(filename:string):string=
+proc cxZDisplayFile*(filename:string,height:int=400,width:int=500):string=
   ## display text file , text can be edited and will be returned edited, file remains unchanged
-  let res = execCmdEx("""zenity --text-info --title="$1" --filename=$1 --editable --no-wrap"""  % filename)
+  let res = execCmdEx("""zenity --text-info --title="$1" --filename=$1 --editable --no-wrap --height=$2  --width=$3"""  % [filename,$height,$width])
   result = res.output
 
 
-proc cxZDisplayHtmlFile*(filename:string):string=
-   ## display html file ,, text can be edited and will be returned edited, file remains unchanged
-   var res = execCmdEx("""zenity --text-info --title="WriteBash.com - demo text-info dialog" --filename=/home/danie/Desktop/test.html --editable --no-wrap --html """)
+proc cxZDisplayHtmlFile*(filename:string,height:int=600,width:int=800):string=
+   ## display html file , text can be edited and will be returned edited, file remains unchanged
+   var res = execCmdEx("""zenity --text-info --title="WriteBash.com - demo text-info dialog" --filename=$1 --editable --no-wrap --html --height=$2  --width=$3"""  % [filename,$height,$width])
    result = res.output
         
 
@@ -174,9 +182,13 @@ proc formExample():seq[string] =
 
 if isMainModule:
   # test var functions
+  
+  var yn = cxZYesNo("Overwrite the existing database.fdb file ")
+  echo yn
+  
   # simple entry dialog
-  var cxz  = cxZDialog("Secret1","Enter a secret",false)
-  echo cxz
+  #var cxz  = cxZDialog("Secret1","Enter a secret",false)
+  #echo cxz
   #var cxz1 = cxZDialog("Secret2","Enter a secret",true) 
   
   #echo cxZdialog("Hello","Say something nice today !",false)
@@ -188,9 +200,9 @@ if isMainModule:
   #echo "Date : ",login.date
   
   
-  #var login1 = cxLoginDialog()
-  #echo "Name : ",login1.name 
-  #echo "Pwd  : ",login1.password
+  var login1 = cxZLoginDialog()
+  echo "Name : ",login1.name 
+  echo "Pwd  : ",login1.password
   
   # small pop ups  
   #cxZinfo()
